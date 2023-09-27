12a90d20af97b543faf5475ac39d0fe5ded29a68
Harmony Height <heightharmony@gmail.com>
2023-09-27 21:46:16 +0500

    navbar added

--- harmony-height/src/app/page.tsx
+++ harmony-height/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <div >
+      <div>
         <div className="navbar bg-base-100">
           <div className="navbar-start">
             <div className="dropdown">
@@ -11,7 +11,7 @@ export default function Home() {
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
               </label>
               <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                <li><a>Item 1</a></li>
+                <li><a>Home</a></li>
                 <li>
                   <a>Category</a>
                   <ul className="p-2">
