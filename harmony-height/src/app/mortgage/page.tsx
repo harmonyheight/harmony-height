ea16e21b290a188263c097e9a82c78dd04295de1
Aruna3456 <anu594958@gmail.com>
2023-11-18 08:17:45 +0500

    working on mortgage

---
+++ harmony-height/src/app/mortgage/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/mortgage/Header";
+import NavBar from "@/components/navbar/NavBar";
+import React from "react";
+
+const page = () => {
+    return (
+        <div>
+            <NavBar />
+            <Header />
+        </div>
+    );
+};
+
+export default page;
