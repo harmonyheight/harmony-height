b5c4157cef93bad3df5507d5c20480e992a4be53
pihupriyanshu <priyanshipihu147@gmail.com>
2023-12-12 00:48:03 +0500

    fixes

--- .scripts/clientdeploy.sh
+++ .scripts/clientdeploy.sh
@@ -3,15 +3,25 @@ set -e
 
 echo "Client Deployment started..."
 
+# Use the correct path to npm
+NPM_PATH=$(which npm)
+
+# Use the correct path to pm2
+PM2_PATH=$(which pm2)
+
+# Navigate to your project directory (replace "/path/to/your/project" with your actual project path)
+cd /path/to/your/project
+
 echo "Installing Dependencies..."
-npm install --yes
+"$NPM_PATH" install --production
 
 echo "Creating Production Build..."
-npm run build
+"$NPM_PATH" run build
 
 echo "PM2 Reload"
-pm2 reload 1
-echo "Retsart Nginx"
+"$PM2_PATH" reload 1
+
+echo "Restart Nginx"
 systemctl restart nginx
 
 echo "Client Side Deployment Finished!"
